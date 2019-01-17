2e6c5ae35039bb9fb0600efa3d7e27e499663617
Sang Young Noh <gnatnoh123@googlemail.com>
2019-01-17 18:40:16 +0000

    added broadcast

--- MPI_work/src/MPI_broadcast.cxx
+++ MPI_work/src/MPI_broadcast.cxx
@@ -9,8 +9,8 @@ MPI_BC::MPI_BC() {
 MPI_BC::~MPI_BC() {
 } // destructor 
 
-int MPI_BC::Ceiling_log2(int  x  /* in */) {
-    /* Use unsigned so that right shift will fill                                                                                                    
+int MPI_BC::Ceiling_log2(int x) {
+    /* Use unsigned so that right shift will fill                                                                                                 
      * leftmost bit with 0 
      */
     unsigned temp = (unsigned) x - 1;
@@ -23,29 +23,24 @@ int MPI_BC::Ceiling_log2(int  x  /* in */) {
     return result;
 } 
 
-void MPI_BC::Send(float  a     /* in */,
-        float  b     /* in */,
-	int    n     /* in */,
-        int    dest  /* in */) {
-
+void MPI_BC::Send(float a, float b, int n, int dest) {
     MPI_Send(&a, 1, MPI_FLOAT, dest, 0, MPI_COMM_WORLD);
     MPI_Send(&b, 1, MPI_FLOAT, dest, 1, MPI_COMM_WORLD);
     MPI_Send(&n, 1, MPI_INT, dest, 2, MPI_COMM_WORLD);
 } /* Send */
 
 
-void MPI_BC::Receive(
-        float*  a_ptr  /* out */,
-	float*  b_ptr  /* out */,
-        int*    n_ptr  /* out */,
-        int     source /* in  */) {
-
-    MPI_Status status;
-
-    MPI_Recv(a_ptr, 1, MPI_FLOAT, source, 0,
-	MPI_COMM_WORLD, &status);
-    MPI_Recv(b_ptr, 1, MPI_FLOAT, source, 1,
-        MPI_COMM_WORLD, &status);
-    MPI_Recv(n_ptr, 1, MPI_INT, source, 2,
-        MPI_COMM_WORLD, &status);
+void MPI_BC::Receive(float* a_ptr, float* b_ptr, int* n_ptr, int     source) {
+    MPI_Recv(a_ptr, 1, MPI_FLOAT, source, 0, MPI_COMM_WORLD, &status);
+    MPI_Recv(b_ptr, 1, MPI_FLOAT, source, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(n_ptr, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
 } /* Receive */
+
+
+void MPI_BC::Get_data2() {
+  if (my_rank == 0) {
+    std::cout << "Enter a, b, and n \n";
+    scanf("%lf %lf %d", a_ptr, b_ptr, n_ptr);
+  }
+  
+}

6bd4f57e095019bc2f253172f7281cac92de75ad
Sang Young Noh <gnatnoh123@googlemail.com>
2019-01-19 22:39:11 +0000

    added mpi main.cxx

--- MPI_work/main.cxx
+++ MPI_work/main.cxx
@@ -1,9 +1,34 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+
+#include <mpi.h>
+#include <iostream>
+
 #include "mpi.h"
 #include "MPI_IO.hpp"
 
-int main () {
+int main() {
+  
+  MPI_Init(&argc, &argv);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+  if (rank == 0) {
+    int value = 17;
+    int result = MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+
+    if (result == MPI_SUCCESS)
+      std::cout << "Rank 0 OK!" << std::endl;
+  } else if (rank == 1) {
+    int value;
+    int result = MPI_Recv(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    if (result == MPI_SUCCESS && value == 17)
+      std::cout << "Rank 1 OK!" << std::endl;
+  }
+  
+  MPI_Finalize();
+
+  return 0;
 }

21af99395abc80d182d46874d927baff3f3d4b1a
Sang Young Noh <gnatnoh123@googlemail.com>
2019-01-21 17:56:10 +0000

    new

--- MPI_work/main.cxx
+++ MPI_work/main.cxx
@@ -13,6 +13,18 @@
 
 std::vector<int> aa;
 
+// Creates an array of random numbers. Each number has a value from 0 - 1
+float *create_rand_nums(int num_elements) {
+  float *rand_nums = (float *)malloc(sizeof(float) * num_elements);
+  assert(rand_nums != NULL);
+  int i;
+  for (i = 0; i < num_elements; i++) {
+    rand_nums[i] = (rand() / (float)RAND_MAX);
+  }
+  return rand_nums;
+}
+
+
 void Read_vector(double* local_a, int local_n, int n, std::string vec_name, int my_rank, MPI_Comm comm) {
   double* a = NULL;
   int i;
@@ -21,6 +33,8 @@ void Read_vector(double* local_a, int local_n, int n, std::string vec_name, int 
   }
 
 }
+
+
 
 int main() {
   MPI_Init(&argc, &argv);
--- MPI_work/src/MPI_broadcast.cxx
+++ MPI_work/src/MPI_broadcast.cxx
@@ -11,6 +11,28 @@ std::map<std::string, std::string> typeConvDict; // TODO
 
 enum MPI_TYPE {MPI_CHAR, MPI_SHORT, MPI_INT, MPI_LONG, MPI_LONG_LONG, MPI_UNSIGNED_CHAR, MPI_UNSIGNED_SHORT, MPI_UNSIGNED, MPI_UNSIGNED_LONG, MPI_FLOAT, MPI_DOUBLE, MPI_LONG_DOUBLE, MPI_BYTE, MPI_PACKED};
 
+
+void my_bcast(void* data, int count, MPI_Datatype datatype, int root,
+              MPI_Comm communicator) {
+  int world_rank;
+  MPI_Comm_rank(communicator, &world_rank);
+  int world_size;
+  MPI_Comm_size(communicator, &world_size);
+
+  if (world_rank == root) {
+    // If we are the root process, send our data to everyone
+    int i;
+    for (i = 0; i < world_size; i++) {
+      if (i != world_rank) {
+        MPI_Send(data, count, datatype, i, 0, communicator);
+      }
+    }
+  } else {
+    // If we are a receiver process, receive the data from the root
+    MPI_Recv(data, count, datatype, root, 0, communicator, MPI_STATUS_IGNORE);
+  }
+}
+
 MPI_BC::MPI_BC() {
   MPI_Init(NULL, NULL);
   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);

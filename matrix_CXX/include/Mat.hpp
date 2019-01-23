1ecbb6efbc9990d2d66fb659456f60d40b232841
Sang Young Noh <gnatnoh123@googlemail.com>
2019-01-23 14:43:05 +0000

    new

---
+++ matrix_CXX/include/Mat.hpp
@@ -0,0 +1,41 @@
+#ifndef __SIMPLE__MATRIX__H__
+#define __SIMPLE__MATRIX__H__
+
+/*
+  Generic Programming and Template Classes
+  
+  In order to explain how generic programming from object-orientated 
+  programming (OOP) we need to review how classes work.  Recall that clas  ses allow encapsulation of data. They provide a declaration of an interface. 
+
+  Normal Classes are bound to the data types specified for their 
+  member data. This means hat if we want to store values or objects within that class, we must specify the type of data upfront 
+
+ 
+*/
+
+
+#include <vector>
+
+template <typename Type = double> class SimpleMatrix {
+public:
+  SimpleMatrix(); // Default constructor
+  // Constructor specifying rows, colummns, and a default value 
+  SimpleMatrix(const int& rows, const int& cols, const Type& val);
+  // Copy constructor
+  SimpleMatrix(const SimpleMatrix<Type>& _rhs);
+  // Assignment operator overloaded
+  virtual ~SimpleMatrix(); // Destructor
+  // Access to the matrix values, directly, via row and column indices
+  std::vector<std::vector<Type> > get_mat() const;
+  Type& value(const int& row, const int& col);
+
+
+private:
+  std::vector<std::vector<Type> > mat; // Use a "vector of vectors to store the values
+};
+
+
+#endif
+
+
+
---
+++ matrix_CXX/src/Mat.cxx
@@ -0,0 +1,18 @@
+#ifndef __SIMPLE_MATRIX_CPP
+#define __SIMPLE_MATRIX_CPP
+
+#include "Mat.hpp"
+
+// Default constructor
+
+template <typename Type>
+SimpleMatrix<Type>::SimpleMatrix() {
+  // No need for implementation, as the vector "mat"
+  // will create the necessary storage  
+}
+
+// Constructor with row/col specification and default values
+
+
+
+#endif

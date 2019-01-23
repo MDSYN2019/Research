aadc7ff56aeac7d327e52666501cc5eeb26d43e9
Sang Young Noh <gnatnoh123@googlemail.com>
2019-01-23 15:55:59 +0000

    Added functions onto the matrix class

--- matrix_CXX/include/Mat.hpp
+++ matrix_CXX/include/Mat.hpp
@@ -7,9 +7,19 @@
   In order to explain how generic programming from object-orientated 
   programming (OOP) we need to review how classes work.  Recall that clas  ses allow encapsulation of data. They provide a declaration of an interface. 
 
-  Normal Classes are bound to the data types specified for their 
-  member data. This means hat if we want to store values or objects within that class, we must specify the type of data upfront 
+  Normal Classes are bound to the data types specified for their member data. This means hat if we want to store values or objects within that class, we must specify the type of data upfront 
 
+  -------------------
+  Generic Programming
+  -------------------
+
+  One common question that arises when discussing templates is "Why use templates over normal object orientation?"
+
+  - Generally, more errors caught at complule time and less at run-time. Thus there isn't 
+  as much need for try-catch exception blocks in your code 
+
+
+  
  
 */
 
@@ -28,7 +38,6 @@ public:
   // Access to the matrix values, directly, via row and column indices
   std::vector<std::vector<Type> > get_mat() const;
   Type& value(const int& row, const int& col);
-
 
 private:
   std::vector<std::vector<Type> > mat; // Use a "vector of vectors to store the values
--- matrix_CXX/src/Mat.cxx
+++ matrix_CXX/src/Mat.cxx
@@ -12,7 +12,39 @@ SimpleMatrix<Type>::SimpleMatrix() {
 }
 
 // Constructor with row/col specification and default values
+template <typename Type>
+SimpleMatrix<Type>::SimpleMatrix(const int& rows, const int& cols, const Type& val) {
+  for (int i = 0; i < rows; i++) {
+    std::vector<Type> col_vec (cols, val);
+    mat.push_back(col_vec);
+  }
+}
 
+// Copy contructor
+template <typename Type>
+SimpleMatrix<Type>::SimpleMatrix(const SimpleMatrix<Type>::operator=(const SimpleMatrix<Type>& _rhs)) {
+  if (this == &_rhs) return *this; // If we point the = to itsself, return the pointer to this 
+  return *this; // else return the pointer to this function
+}
 
+// Destructor
+template <typename Type>
+SimpleMatrix<Type>::~SimpleMatrix() {
+  // No need for implementation, as there is no
+  // manual dynamic memory allocation
+}
+
+// Matrix access method, via copying
+template <typename Type>
+SimpleMatrix<Type> SimpleMatrix<Type>::get_mat() const {
+  return mat;
+}
+
+// Matrix access method, via rows and columns index
+template <typename Type>
+Type& SimpleMatrix<Type>::value(const int& row, const int& col) {
+
+  return mat[row][col];
+}
 
 #endif

a6f4705bc34f78d553f27b2ce4d877a443c2df04
Sang Young Noh <gnatnoh123@googlemail.com>
2019-01-23 14:27:26 +0000

    Added CMake file

--- matrix_CXX/include/PayOff.hpp
+++ matrix_CXX/include/PayOff.hpp
@@ -1,6 +1,13 @@
 #ifndef __PAYOFF__
 #define __PAYOFF__
 
+/*
+
+Inheritance is a powerful concept in object-orientated programming (OOP).
+It allow us to model a type of relationship between object known as is-a.
+
+*/
+
 class PayOff {
 public:
   PayOff(); // Default (no parameter) constructor 
@@ -22,6 +29,19 @@ private:
   double D;
 public:
   PayOffDoubleDigital(const double U_, const double D_);
+
+  /*
+    Virtual destructors
+
+    All of our PayOff class and subclass and subclass destrutors have so 
+    far been set to virtual, using the prefixed virtual keyword.
+    
+    In simple terms, a virtual destructor ensures that when derived 
+    subclasses go our of scope, 
+    
+    
+   */
+
   virtual ~PayOffDoubleDigital();
   // Pay-off is 1 if spot within strike barriers, 0 otherwise
   virtual double operator() (const double S) const; 

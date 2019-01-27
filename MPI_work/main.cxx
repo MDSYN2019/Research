f64749b855fb56eb955edffe6a5b6af16ce230f4
Sang Young Noh <gnatnoh123@googlemail.com>
2019-01-27 17:04:35 +0000

    MPI_bcast

--- MPI_work/main.cxx
+++ MPI_work/main.cxx
@@ -7,72 +7,73 @@
 
 #include "mpi.h"
 
+typedef struct {
+  double a;
+  double b;
+  int c;
+} myStruct;
+
+
 double total_d, local_d;
 
-void Build_mpi_type (double* a_p, double* b_p, int* n_p, MPI_Datatype* input_mpi_t_p) {
+void Build_mpi_type (double* a_p, double* b_p, int* n_p, myStruct* stct, MPI_Datatype* input_mpi_t_p) {
+
   int array_of_blocklengths[3] = {1,1,1};
   MPI_Datatype array_of_types[3] = {MPI_DOUBLE, MPI_DOUBLE, MPI_INT};
   MPI_Aint array_of_displacements[3] = {0};
   MPI_Aint a_addr, b_addr, n_addr;
 
-  MPI_Get_address(a_p, &a_addr);
-  MPI_Get_address(b_p, &b_addr);
-  MPI_Get_address(n_p, &n_addr);
+  (*stct).a = *a_p;
+  (*stct).b = *b_p;
+  (*stct).c = *n_p;
+  
+  MPI_Get_address(&stct->a, &a_addr);
+  MPI_Get_address(&stct->b, &b_addr);
+  MPI_Get_address(&stct->c, &n_addr);
   
   array_of_displacements[1] = b_addr - a_addr;
   array_of_displacements[2] = n_addr - a_addr;
   MPI_Type_create_struct(3, array_of_blocklengths, array_of_displacements, array_of_types, input_mpi_t_p);
   MPI_Type_commit(input_mpi_t_p);
-
 }
 
-void Get_Input(int my_rank, int comm_sz, double* a_p, double* b_p, int* n_p) {
+void Get_Input(int my_rank, int comm_sz, double* a_p, double* b_p, int* n_p, myStruct* myStruct) {
+
   MPI_Datatype input_mpi_t;  
-  Build_mpi_type(a_p, b_p, n_p, &input_mpi_t);
+  Build_mpi_type(a_p, b_p, n_p, myStruct, &input_mpi_t);
+ 
   if (my_rank == 0) {
-    std::cout << "Enter a, b and n \n";
-    scanf("%lf %lf %d", a_p, b_p, n_p);
-    MPI_Bcast(a_p, 1, input_mpi_t, 0, MPI_COMM_WORLD);
-  } else {    
-    total_d = *a_p;
-    for (int source = 1; source < comm_sz; source++) {
-      MPI_Recv(&a_p, 1, input_mpi_t, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      std::cout << total_d << std::endl;
-      total_d += total_d;
-    }
+    MPI_Bcast(myStruct, 1, input_mpi_t, 0, MPI_COMM_WORLD);
   }
-
+  
   MPI_Type_free(&input_mpi_t);
-
-
 }
-
 
 // MPI headers
 
 #include "MPI_IO.hpp"
 //#include "MPI_broadcast.hpp"
 //#include "MPI_functions.hpp"
-
 std::vector<int> bb;
+myStruct exampleDatatype;
+
+double a;
+double b;
+int c;
 
 int main(int argc, char** argv) {
-  double a = 3.0;
-  double b = 4.0;
-  int c = 5;
-
   int my_rank, comm_sz;
   MPI_Init(NULL, NULL);
   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-  Get_Input(0, comm_sz, &a, &b, &c);
-
+  std::cout << "Enter a, b and n \n";
+  scanf("%lf %lf %d", &a, &b, &c);
+ 
+  Get_Input(0, comm_sz, &a, &b, &c, &exampleDatatype);
   for (int q = 1; q < comm_sz; q++) {
-    std::cout << q << a << b << c << std::endl;
+    std::cout << q << " " << exampleDatatype.a << " " << exampleDatatype.b << " " << exampleDatatype.c << std::endl;
   }
-
-  std::cout << total_d << std::endl;
   
   MPI_Finalize();
   //MPI_input a(3, 5);

205e2622fcf5d6a7e6b1559996a0d5b42cdcf56d
Sang <gnatnoh123@googlemail.com>
2019-03-02 21:13:30 +0000

    new

--- MPI_work/new.hpp
+++ MPI_work/new.hpp
@@ -19,15 +19,25 @@ public:
   virtual ~InitiateVectorMethod() {};
 
   // void public  methods
-  void setup(int* ); 
+
+  void setup(int*); 
   void traits();
   void SendVector();
-
 private:
   int my_rank, comm_sz;
   std::deque<ItemType> A;
   int var1, var2;
+  
 };
 
 #endif
 
+/*
+
+Pack/Unpack
+
+An alternative approach to grouping data is provided by the MPI functions MPI_Pack 
+and MPI_unpack functions. MPI_Pack allows one to explicitly store noncintiguous data in 
+contiguous memory locations 
+
+ */

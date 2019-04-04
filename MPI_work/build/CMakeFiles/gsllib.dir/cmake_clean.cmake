1f5a2186a18d7e658c4ccd5ce0407958755e064a
Sang <gnatnoh123@googlemail.com>
2019-04-04 16:07:25 +0100

    new

---
+++ MPI_work/build/CMakeFiles/gsllib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gsllib.dir/main.o"
+  "libgsllib.pdb"
+  "libgsllib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gsllib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

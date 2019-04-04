2cd45c298f1864977e08c744187817f740d2dbbe
Sang <gnatnoh123@googlemail.com>
2019-04-04 19:15:25 +0100

    new

--- OPENMP_work/src/openmp2.cxx
+++ OPENMP_work/src/openmp2.cxx
@@ -11,7 +11,6 @@
 
  */
 
-class 
 
 void Trap(double a, double b, int n, double* global_result_p);
 
@@ -20,7 +19,33 @@ int main(int argc, char* argv[]) {
   double a, b;
   int n;
   int thread_count;
-
+  Trap(a, b, n, &global_result);
+  
   
   return 0;
+}
+
+void Trap(double a, double b, int n, double* global_result_p) {
+  double h, x, my_result;
+  double local_a, local_b;
+  int i, local_n;
+
+  int my_rank = omp_get_thread_num();
+  int thread_count = omp_get_num_threads();
+
+  h = (b -a)/n;
+  local_n = n / thread_count; 
+  local_a = a + my_rank * local_n * h;
+  local_b = local_a + local_n * h;
+
+  my_result = ((float)local_a + (float)local_b)/2.0;
+
+  for (unsighed int i = 1; i <= local_n - 1; i++) {
+    x = local_a + i * h;
+    my_result += (float)x;
+  }
+
+  my_result = my_result * h;
+  # pramga omp critical
+  *global_result_p += my_result;
 }

d05bbe7941cfabad6c7cfff36835ec178c19010e
Sang <gnatnoh123@googlemail.com>
2019-04-12 17:19:49 +0100

    new

---
+++ NEQFREE/main.cxx
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include "mpi.h"
+#include "MPI_broadcast.hpp"
+
+MPI_BC AA;
+
+int main () {
+  AA.add_vector();
+  //  AA.broadcast_vector();
+  AA.vectorTest();
+
+  return 0;
+}

a5fbcb38f3a5225a4dd35fdc0c6041423e10fef1
Sang <gnatnoh123@googlemail.com>
2019-04-12 19:01:28 +0100

    Added build.sh #

---
+++ MPI_work/build/CMakeFiles/MPI_EX.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/oohnohnoh1/Desktop/GIT/PhD_work/MPI_work/main.cxx" "/home/oohnohnoh1/Desktop/GIT/PhD_work/MPI_work/build/CMakeFiles/MPI_EX.dir/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../include"
+  "/home/oohnohnoh1/anaconda3/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/oohnohnoh1/Desktop/GIT/PhD_work/MPI_work/build/src/CMakeFiles/src.dir/DependInfo.cmake"
+  "/home/oohnohnoh1/Desktop/GIT/PhD_work/MPI_work/build/CMakeFiles/gsllib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build.sh
@@ -0,0 +1,4 @@
+#!bin/sh
+cd $TRAVIS_BUILD_DIR/MPI_work/build
+cmake ../
+

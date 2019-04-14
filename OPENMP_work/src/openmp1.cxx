43d159b150ce6397d67d443dcb3b2973d8dfd2fd
Sang <gnatnoh123@googlemail.com>
2019-04-14 16:30:44 +0100

    new

--- OPENMP_work/src/openmp1.cxx
+++ OPENMP_work/src/openmp1.cxx
@@ -1,26 +1,50 @@
-#include <iostream>
-#include <cstdlib>
-#include <cstdio>
-#include <omp.h>
-
 /*
-  OpenMP provides what's known as a "directives-based" sahred memory API. In C and C++, this means that ther are special preprocessor instrutions
-  known as pragmas. Pragmas are typically added to a system to allow behaviours that aren't part of the basic C specification. 
+OpenMP - Open multi processing
 
-  Compilers that dont support the pragmas are free to ignore them. This allows a program that uses the pragmas to run on platforms that
-  don't support them.
+Pthreads requires the programmer to explicitly epecifiy the behaviour of each thread. 
+
+OpenMP (Open multi-processing), on the other hand, sometimes allows the programmer 
+to simply state that a block fo code should be exectuted in parallel, and the precise 
+determination of the takss and which thread should execute them is left to the compiler and 
+the run-time system
+
+OpenMP is a way to program shared memory devices. This means that the parellism occurs where every parallel thread has access 
+to all your data 
+
+Example:
+You can think of it as: parallelism can appen during the execution of a specific for loop by splittig up the loop
+among the different threads.
+
+
+OpenMP provides hat's known as a "directives-based" shared memory API. In C and C++, this means that there are speciial preprocessor 
+instructions known as pragmas. Pragmas are typcailyl added to a system to allow behaviours that aren't part of the basic C specification
+
 
  */
 
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
 
+// Instead of just calling the OpenMP functions, e can first check whetehr _OPENMP is defined.
+
+#ifndef _OPENMP
+#include <omp.h>
+#endif
 
 template <class T> class Vec {
 public:
   // Interface
   // Two constructors defined here -
+  // We need to provide typedefs for the const and nonconst iterator types.
+  
   Vec() {create();} // Default constructor 
   explicit Vec(std::size_t n, const T& = T()) { create(n, val);} // When we say that a constructor is explicit, we're saying that the compiler
                                                                  // will use specifically invokes the constructor
+
+  typedef T* iterator;
+  typedef const T* const_iterator
+  typedef size_t size_type;
   
 private:
   // Implemntation
@@ -28,7 +52,14 @@ private:
   T* limit;
 };
 
+void Trap(double a, double b, int n, double * global_result_p) {
+  double h, x, my_result;
+  double local_a, local_b;
+  int i, local_n;
 
+  int my_rank = omp_get_thread_num();
+  int thread_count = omp_get_num_threads();
+}
 
 /*
   
@@ -81,31 +112,23 @@ void Hello(void) {
   int my_rank = omp_get_thread_num();
   int thread_count = omp_get_num_threads();
   std::cout << "Hello from thread " << my_rank  << " of " <<  thread_count << std::endl;
-}
-
-void Trap(double a, double b, int n, double * global_result_p) {
-
-  double h, x, my_result;
-  double local_a, local_b;
-  int i, local_n;
-
-  int my_rank = omp_get_thread_num();
-  int thread_count = omp_get_num_threads();
+  // Each thread runs the Hello() function  -> Which simply prints out a string as following above.
 }
 
 class OMP {
 public:
   OMP();
-  ~OMP();
-  
+  ~OMP();  
   int input;
-  
 private:
-  int my_rank = omp_get_thread_num();
-  int thread_count = omp_get_num_threads();
+  int my_rank; // 
+  int thread_count;
   
 };
   
 
-
+OMP::OMP() {
+# pragma omp parallel num_threads(thread_count) // OpenMP directive - the program should start some threads. Each thread that's forked
+                                                // should execute the Hello function, and when the threads return from the call to Hello,
+}
 

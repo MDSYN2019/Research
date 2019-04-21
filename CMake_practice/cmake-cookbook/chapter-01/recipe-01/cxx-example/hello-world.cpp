65f3c282b4f6745c3db400988cfb263888885005
Sang <gnatnoh123@googlemail.com>
2019-04-21 19:47:28 +0100

    working on some basics..

--- CMake_practice/cmake-cookbook/chapter-01/recipe-01/cxx-example/hello-world.cpp
+++ CMake_practice/cmake-cookbook/chapter-01/recipe-01/cxx-example/hello-world.cpp
@@ -1,8 +1,11 @@
+#include "Message.hpp"
 #include <cstdlib>
 #include <iostream>
 #include <string>
 
 std::string say_hello() { return std::string("Hello, CMake world!"); }
+
+
 
 int main() {
   std::cout << say_hello() << std::endl;

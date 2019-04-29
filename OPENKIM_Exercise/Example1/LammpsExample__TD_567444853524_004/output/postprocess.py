1db5e4ef6a1557b5524c2e5be01146ee12a90c44
Sang <gnatnoh123@googlemail.com>
2019-04-29 20:54:37 +0100

    new

--- OPENKIM_Exercise/Example1/LammpsExample__TD_567444853524_004/output/postprocess.py
+++ OPENKIM_Exercise/Example1/LammpsExample__TD_567444853524_004/output/postprocess.py
@@ -79,8 +79,8 @@ KIMMODELSLIST = [
 	
 TemplatePath = os.path.abspath('../')
 CurrentPath = os.path.abspath('.')
-os.mkdir(str(template_path + "/" + "output")) # Make output directory 
 
+#os.mkdir(str(TemplatePath + "/" + "output")) # Make output directory 
 # Parser to read in the force fields, the Lattice constant, log file and the lammps binary of the Computer
 
 parser = argparse.ArgumentParser(description="Stdin for OpenKIM")
@@ -88,25 +88,35 @@ parser.add_argument('--Forcefield', action = 'store' , type = str, help = 'The n
 parser.add_argument('--Lattice_Constant', action = 'store', type = float, help = 'The value of the lattice constant')
 parser.add_argument('--Log', action = 'store', type = str, help = 'Name of the log file')
 parser.add_argument('--LAMMPS_binary', action='store', type = str, help = 'Path to lammps binary')
-args = parser.parse_args()
-print (args)
+print (parser.parse_args(['--Forcefield', '--Lattice_Constant', '--Log', '--LAMMPS_binary']))
 
 # Making sure of errors
 
+# Make sure that the input Forcefield exists
+
 try:
-	args.Forcefield in KIM_MODELS_LIST
+	args.Forcefield in KIMMODELSLIST
 except IndexError:
 	print ("Input forcefield is not valid")
 
-# Could try to use the python version of LAMMPS here for exceptions 
-
+# Testing that LAMMPS works through a subprocess
 try:
 	proc = subprocess.Popen("/home/oohnohnoh1/Desktop/LAMMPS/lammps-12Dec18/src/lmp_ubuntu", shell=True) 
 	p_id = psutil.Process(proc.pid)
-	p_id.wait(timeout = 3)
+	p_id.kill()
+	print ("LAMMPS works!")
 except subprocess.CalledProcessError as e:
 	print(e.output)
 
+# Testing whether log.lammps output is in the output folder 
+
+try:
+	FileOpen = open(CurrentPath + "/" + "log.lammps")
+	print ("log.lammps exists")
+except FileNotFoundError:
+	print ("log.lammps does not exist")
+
+	
 class KIMPostprocess:
 	"""
 

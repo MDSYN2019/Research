2639f72eb2da6e8fd785b4114071ddf02185dcc9
Sang <gnatnoh123@googlemail.com>
2019-06-06 22:18:15 +0100

    new

--- OPENMP_Numerical_Methods/main.cxx
+++ OPENMP_Numerical_Methods/main.cxx
@@ -40,6 +40,7 @@ double f (int i) {
 //OMP AA(5);
 
 int main () {
+
   Matrix2d a;
   a << 1, 2,
        3, 4;
@@ -48,7 +49,18 @@ int main () {
        1, 4;
   std::cout << "a + b =\n" << a + b << std::endl;
 
+  // Computing the inverse and the determinant
+
+  Matrix3f A;
+
+  A << 1, 2, 1,
+    2, 1, 0,
+    -1, 1, 2;
   
+  std::cout << "Here is the matrix A:\n" << A << std::endl;
+  std::cout << "The determinant of A is " << A.determinant() << std::endl;
+  std::cout << "The inverse of A is:\n" << A.inverse() << std::endl;
+ 
   /*
   int x = 5;
 

3569de9eb64a19d9ce2cc167064a9c78be0f1146
Sang <gnatnoh123@googlemail.com>
2019-06-08 19:51:11 +0100

    new

---
+++ OPENMP_Numerical_Methods/src/openmp_LA.cxx
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <memory>
+
+#ifndef _OPENMP_
+#include <omp.h>
+
+#endif
+
+// Main header to include 
+
+#include "openmp1.hpp"
+
+// QAT headers
+
+#include "Argument.h"
+
+/* CPPunit tests */
+
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+

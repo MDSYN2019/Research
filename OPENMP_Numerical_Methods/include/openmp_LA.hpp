8c836814f31858d513d2a5281a166a8afb1c23d4
Sang <gnatnoh123@googlemail.com>
2019-06-08 20:01:58 +0100

    new

---
+++ OPENMP_Numerical_Methods/include/openmp_LA.hpp
@@ -0,0 +1,21 @@
+/* CPPunit tests */
+
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+
+// Eigen Library
+
+#include "Eigen/Dense"
+#include "Eigen/LU"
+
+typedef Eigen::Matrix<double, 4, 4> Matrix4x4;
+
+class SYN_Mat {
+public:
+  
+private:
+};
--- OPENMP_Numerical_Methods/src/openmp_LA.cxx
+++ OPENMP_Numerical_Methods/src/openmp_LA.cxx
@@ -15,6 +15,7 @@
 // Main header to include 
 
 #include "openmp1.hpp"
+#include "openmp_LA.hpp"
 
 // QAT headers
 
@@ -27,4 +28,12 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
+
+// Eigen Library
+
+#include "Eigen/Dense"
+#include "Eigen/LU"
+
+typedef Eigen::Matrix<double, 4, 4> Matrix4x4;
+
 

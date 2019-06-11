c539529932a0493037a46a7694b0fac5b9704263
Sang <gnatnoh123@googlemail.com>
2019-06-11 17:26:02 +0100

    new

--- OPENMP_Numerical_Methods/src/openmp2.cxx
+++ OPENMP_Numerical_Methods/src/openmp2.cxx
@@ -3,25 +3,15 @@
 
 #include <exception>
 #include <iostream>
+#include <deque>
+#include <thread>
 
-int add(int i, int j) {
-    return i + j;
-}
+//template <typename T, typename Container = std::deque<T> >
+//class stack {
+//public:
+//private:
+//};
 
-/*
-PYBIND11_MODULE(example, m) {
-    m.doc() = "pybind11 example plugin"; // optional module docstring
-    m.def("add", &add, "A function which adds two numbers");
-}
-*/
-
-/*
-class Core {
-public:
-  Core() : midterm
-private:
-};
-*/
 
 class Complex { 
   friend bool operator ==(const Complex& a, const Complex& b);

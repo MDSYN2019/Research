ddba4965b0bdc60647cbbbf58ddb8ce8ec204ca9
Sang <gnatnoh123@googlemail.com>
2019-06-12 00:53:32 +0100

    new

--- OPENMP_Numerical_Methods/src/openmp_LA.cxx
+++ OPENMP_Numerical_Methods/src/openmp_LA.cxx
@@ -96,7 +96,12 @@ double ProbDist<T>::d_j(const int& j, const double& S, const double& K, const do
   return (log(S/K) + (r + (pow(-1, j-1) * 0.5 * v * v) * T)/(v * (pow(T, 0.5))));
 }
 
+// TODO 
+double call_price(const double& S, const double& K, const double& r, const double& v, const double& T) {
+  return S * this->norm_cdf(d_j(1, S, K, r, v, T)) + K * exp(-r*T) * this->norm_cdf(-d_j(2, S, K, r, v, T));
+}
+
 double ProbDist<T>::put_price(const double& S, const double& K, const double& r, const double& v, const double& T) {
-  return (-S * norm_cdf(-d_j(1, S, K, r, v, T)) + K*exp(r*T) * norm_cdf(-d_j(2, S, K, v, T))); 
+  return (-S * this->norm_cdf(-d_j(1, S, K, r, v, T)) + K*exp(r*T) * this->norm_cdf(-d_j(2, S, K, v, T))); 
 }
 

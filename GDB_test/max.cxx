b9ff81ef96bcfe65f4d392c91b1fa3dbd968cdde
Sang <gnatnoh123@googlemail.com>
2019-08-12 20:21:21 +0100

    new

--- GDB_test/max.cxx
+++ GDB_test/max.cxx
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include "max.hpp"
+#include "classtemp.hpp"
 
 int main(void) {
   int i = 42;
@@ -9,6 +10,9 @@ int main(void) {
 
   double f1 = 3.4;
   double f2 = -6.7;
+
+  ::max(7, 42, 68);
+  ::max(7.0, 42.0);
   
 
   return 0;

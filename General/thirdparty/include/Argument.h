02d324204b810aa189783713a5d668bfe5c62f9f
Sang <gnatnoh123@googlemail.com>
2019-08-18 23:19:05 +0100

    new

--- General/thirdparty/include/Argument.h
+++ General/thirdparty/include/Argument.h
@@ -1,4 +1,4 @@
-//---------------------------------------------------------------------------//
+//----------------------------------------------------------------------------//
 // !!                                                                     !! //
 //                                                                           //
 //  Copyright (C) 2016 Joe Boudreau                                          //
@@ -35,39 +35,26 @@
 #include <initializer_list>
 
 namespace Genfun {
-
   class Argument {
-
   public:
-
     // Constructor
     Argument(int ndim=0);
-
     // Copy Constructor
     Argument( const Argument &);
-
     // Construct from initializer list:
     Argument(std::initializer_list<double> );
-
     // Assignment operator
     const Argument & operator=(const Argument &);
-
     // Destructor:
     ~Argument();
-
     // Set/Get Value
     double & operator[] (int I);
     const double & operator[] (int i) const; 
-
     // Get the dimensions
     unsigned int dimension() const;
-
   private:
-
     std::vector<double> *_data;
-
     friend std::ostream & operator << (std::ostream & o, const Argument & a);
-
   };
 
   inline Argument::Argument(const Argument & right):
@@ -77,7 +64,7 @@ namespace Genfun {
   inline const Argument & Argument::operator=( const Argument & right) {
     if (this != &right) {
       delete _data;
-      _data=NULL;
+      _data= NULL;
       _data = new std::vector<double>(*(right._data));
     }
     return *this;
@@ -89,30 +76,25 @@ namespace Genfun {
 
   inline double & Argument::operator[] (int i) {
     return (*_data)[i];
-  } 
-
+  }
+  
   inline const double & Argument::operator[] (int i) const {
     return (*_data)[i];
-  } 
-
+  }
+  
   inline Argument::Argument(int ndim): _data(new std::vector<double>(ndim)) {
   }
-
   inline Argument::~Argument() {
     delete _data;
   }
-
   // Construct from initializer list:
   inline Argument::Argument(std::initializer_list<double> lst):_data(new std::vector<double>(lst)) {
   }
-
 
   inline std::ostream & operator << (std::ostream & os, const Argument & a) {
     std::ostream_iterator<double> oi(os,",");
     std::copy (a._data->begin(),a._data->end(),oi);
     return os;
   }
-
-
 } // namespace Genfun
 #endif

72a9b0e3f71bdc4ef7112ac348075b73e1866d41
Sang <gnatnoh123@googlemail.com>
2019-08-26 19:09:48 +0100

    new

--- Parallel/src/MPI_IO.cxx
+++ Parallel/src/MPI_IO.cxx
@@ -178,7 +178,9 @@ void MPIInput::getData(int* start_inp, int* end_inp, int* n_ptr) {
     for (int i = start; i <= end; i++) {
       intVec.push_back(i);
     }
-    
+    int vecSize = intVec.size();
+
+    if (my_rank == 0) {
     for (int dest = 1; dest < p; dest++) { // Looping over the number of processes
       tag = 0;
       MPI_Send(a_ptr, 1, MPI_FLOAT, dest, tag, MPI_COMM_WORLD);
@@ -188,18 +190,19 @@ void MPIInput::getData(int* start_inp, int* end_inp, int* n_ptr) {
       MPI_Send(n_ptr, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
       // Send vectors
       tag = 3;
-      MPI_Send(&intVec[0], intVec.size(), MPI_INT, dest, tag, MPI_COMM_WORLD); // This should point to the address of the first 
+      MPI_Send(&intVec[0], vecSize, MPI_INT, dest, tag, MPI_COMM_WORLD); // This should point to the address of the first 
                                                                                // element of the vector 
     }
   } else {
-    
     tag = 0; // The purpose of the tag
     MPI_Recv(a_ptr, 1, MPI_FLOAT, source, tag, MPI_COMM_WORLD, &status);
     tag = 1;
     MPI_Recv(b_ptr, 1, MPI_FLOAT, source, tag, MPI_COMM_WORLD, &status);
     tag = 2;
     MPI_Recv(n_ptr, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
-  }
+    tag = 3;
+    MPI_Recv(&intVec[0], vecSize, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
+    }
 }
 
 //! Class destructor for MPI

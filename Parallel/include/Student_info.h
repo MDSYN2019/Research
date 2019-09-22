5120b70f2793c6fe7f7f0662b43d7c2f4fc434bd
Sang <gnatnoh123@googlemail.com>
2019-09-22 20:18:11 +0100

    new

--- Parallel/include/Student_info.h
+++ Parallel/include/Student_info.h
@@ -10,32 +10,32 @@
 
 class Student_info {
 public:
-	// constructors and copy control
-	Student_info(): cp(0) { }
-	Student_info(std::istream& is): cp(0) { read(is); }
-	Student_info(const Student_info&);
-	Student_info& operator=(const Student_info&);
-	~Student_info() { delete cp; }
-
-	// operations
-	std::istream& read(std::istream&);
-
-	std::string name() const {
-		if (cp) return cp->name();
-		else throw std::runtime_error("uninitialized Student");
+  // constructors and copy control
+ Student_info(): cp(0) { }
+ Student_info(std::istream& is): cp(0) { read(is); }
+  Student_info(const Student_info&);
+  Student_info& operator=(const Student_info&);
+  ~Student_info() { delete cp; }
+  
+  // operations
+  std::istream& read(std::istream&);
+  
+  std::string name() const {
+    if (cp) return cp->name();
+    else throw std::runtime_error("uninitialized Student");
 	}
-	double grade() const {
-		if (cp) return cp->grade();
-		else throw std::runtime_error("uninitialized Student");
-	}
-
-	static bool compare(const Student_info& s1,
-	                    const Student_info& s2) {
-		return s1.name() < s2.name();
-	}
-
+  double grade() const {
+    if (cp) return cp->grade();
+    else throw std::runtime_error("uninitialized Student");
+  }
+  
+  static bool compare(const Student_info& s1,
+		      const Student_info& s2) {
+    return s1.name() < s2.name();
+  }
+  
 private:
-	Core* cp;
+  Core* cp;
 };
 
 #endif

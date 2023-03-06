16cec908331c804490a326272c32095df035f79f
MDSYN2019 <sangyoung123@googlemail.com>
2023-03-06 15:46:24 +0000

    updated and renamed files

---
+++ free_energy/JarzynskiEqualityComputation/include/Bennet.hpp
@@ -0,0 +1,13 @@
+#ifndef __MBAR__
+#define __MBAR__
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "boost/tuple/tuple.hpp"
+#include "boost/tuple/tuple_io.hpp"
+
+
+
+#endif 
